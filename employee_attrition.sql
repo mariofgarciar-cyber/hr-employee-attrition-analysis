92809a470c084c0fad4a5d39342c434b6c98d7d4
mariofgarciar-cyber <mariof.garciar@gmail.com>
2026-06-26 18:27:01 -0500

    Add files via upload

---
+++ employee_attrition.sql
@@ -0,0 +1,6 @@
+Attrition Rate: 16.1%.
+Sales presenta la mayor tasa de rotación.
+Los empleados con OverTime tienen una tasa de rotación cercana al triple.
+Los empleados que renuncian tienen menor satisfacción laboral.
+Los empleados que renuncian tienen menor salario promedio.
+Los empleados que renuncian son, en promedio, más jóvenes.
